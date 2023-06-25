2b3d7167b333b6b9ed0195d68cda475c6d5f59da
nitin-kukreti <59364817+nitin-kukreti@users.noreply.github.com>
2023-06-25 20:05:57 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ProductReducer from "../products/productSlice"
+// this is store for redux
 const store=configureStore({
     reducer:{
         product:ProductReducer
